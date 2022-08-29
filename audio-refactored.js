15bab43ec32d25ced9bd8241317c84f0239dab16
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-08-29 17:57:06 +0800

    Update audio-refactored.js

--- audio-refactored.js
+++ audio-refactored.js
@@ -49,63 +49,63 @@ const workspace = global_area.getElementsByClassName('workspace')
     const input = Array.from(scope.getElementsByTagName('input')) // get the inputs
     const output_area = scope.getElementsByClassName('output-area')[0] // get the message output area
     const radio_button = {}, number_input = {}
+    const current_value_keeper = new value_keeper
     {
         const r = radio_button, n = number_input
-        {
-            let p = input.slice(0, 3) // radio button precursor
-            for (let i = 0; i < p.length; ++i) { r[i] = p[i], r[p[i].name] = {} } // index type 1 of 2: number; get ready for index type 2
-            for (let i = 0; i < p.length; ++i) { r[p[i].name][p[i].value] = p[i] } // index type 2 of 2: input.name and input.value
-            p = input.slice(3) // number input precursor
-            for (let i = 0; i < p.length; ++i) { n[i] = n[p[i].name] = p[i] }  // 2 types of indices: number and string
-        }
-    }
-    const current_value_keeper = new value_keeper
-    { // add event handlers
+
+        // The purpose of dual indices (compared with just number index): Lessen the modification of code when more inputs are inserted
+        let p = input.slice(0, 3) // radio button precursor
+        for (let i = 0; i < p.length; ++i) { r[i] = p[i], r[p[i].name] = {} } // index type 1 of 2: number; get ready for index type 2
+        for (let i = 0; i < p.length; ++i) { r[p[i].name][p[i].value] = p[i] } // index type 2 of 2: input.name and input.value
+        p = input.slice(3) // number input precursor
+        for (let i = 0; i < p.length; ++i) { n[i] = n[p[i].name] = p[i] }  // 2 types of indices: number and string
+
+        // add event handlers
         const v = current_value_keeper
         const m = new Map // Element-Handler Map: Add event listeners. Canonical units: V, A, Ω, W (i.e., VA)
-        m.set(this.UE_input, () => { // Here we primarily make Z fixed when U has changed
-            this.IE = this.UE / this.Z
-            this.S = this.UE * this.IE
-            this.convert()
+        m.set(n['voltage'], () => { // Here we primarily make Z fixed when U has changed
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert()
         })
-        m.set(this.IE_input, () => { // Here we primarily make Z fixed when I has changed
-            this.UE = this.IE * this.Z
-            this.S = this.UE * this.IE
-            this.convert()
+        m.set(n['current'], () => { // Here we primarily make Z fixed when I has changed
+            v.UE = v.IE * v.Z
+            v.S = v.UE * v.IE
+            v.convert()
         })
-        m.set(this.Z_input, () => { // Here we primarily make U fixed when Z has changed
-            this.IE = this.UE / this.Z
-            this.S = this.UE * this.IE
-            this.convert_W()
+        m.set(n['impedance'], () => { // Here we primarily make U fixed when Z has changed
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert_W()
         })
-        m.set(this.S_input, () => { // Here we primarily make Z fixed and let U, I able to change when S has changed
-            this.UE = Math.sqrt(this.S * this.Z)
-            this.IE = this.UE / this.Z
-            this.convert()
+        m.set(n['power'], () => { // Here we primarily make Z fixed and let U, I able to change when S has changed
+            v.UE = Math.sqrt(v.S * v.Z)
+            v.IE = v.UE / v.Z
+            v.convert()
         })
-        m.set(this.dBV_input, () => {
-            this.UE = Math.pow(10, this.dBV / 20)
-            this.IE = this.UE / this.Z
-            this.S = this.UE * this.IE
-            this.convert()
+        m.set(n['dBV'], () => {
+            v.UE = Math.pow(10, v.dBV / 20)
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert()
         })
-        m.set(this.dBu_input, () => {
-            this.UE = Math.pow(10, (this.dBu + 20 * Math.log10(Math.sqrt(0.001 * 600))) / 20)
-            this.IE = this.UE / this.Z
-            this.S = this.UE * this.IE
-            this.convert()
+        m.set(n['dBu'], () => {
+            v.UE = Math.pow(10, (v.dBu + 20 * Math.log10(Math.sqrt(0.001 * 600))) / 20)
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert()
         })
-        m.set(this.dBW_input, () => {
-            this.S = Math.pow(10, this.dBW / 10)
-            this.UE = Math.sqrt(this.S * this.Z)
-            this.IE = this.UE / this.Z
-            this.convert()
+        m.set(n['dBW'], () => {
+            v.S = Math.pow(10, v.dBW / 10)
+            v.UE = Math.sqrt(v.S * v.Z)
+            v.IE = v.UE / v.Z
+            v.convert()
         })
-        m.set(this.dBm_input, () => {
-            this.S = Math.pow(10, (this.dBm - 30) / 10)
-            this.UE = Math.sqrt(this.S * this.Z)
-            this.IE = this.UE / this.Z
-            this.convert()
+        m.set(n['dBm'], () => {
+            v.S = Math.pow(10, (v.dBm - 30) / 10)
+            v.UE = Math.sqrt(v.S * v.Z)
+            v.IE = v.UE / v.Z
+            v.convert()
         })
         for (const [ e, h ] of m) {
             e.addEventListener('input', (event) => {
