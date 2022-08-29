62ab298d4776953170af3c1452dcce9e95ce0cfd
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-08-29 18:10:10 +0800

    Update audio-refactored.js

--- audio-refactored.js
+++ audio-refactored.js
@@ -51,78 +51,80 @@ const workspace = global_area.getElementsByClassName('workspace')
     const radio_button = {}, number_input = {}
     const current_value_keeper = new value_keeper
     {
-        { // The purpose of dual indices (compared with just number index): Lessen the modification of code when more inputs are inserted
-            const r = radio_button, n = number_input
-            let p = input.slice(0, 3) // radio button precursor
-            for (let i = 0; i < p.length; ++i) { r[i] = p[i], r[p[i].name] = {} } // index type 1 of 2: number; get ready for index type 2
-            for (let i = 0; i < p.length; ++i) { r[p[i].name][p[i].value] = p[i] } // index type 2 of 2: input.name and input.value
-            p = input.slice(3) // number input precursor
-            for (let i = 0; i < p.length; ++i) { n[i] = n[p[i].name] = p[i] }  // 2 types of indices: number and string
-        }
-        { // add event handlers for the current value keeper
-            const v = current_value_keeper
-            const n = number_input
-            const m = new Map // Element-Handler Map: Add event listeners. Canonical units: V, A, Ω, W (i.e., VA)
-            m.set(n['voltage'], () => { // Here we primarily make Z fixed when U has changed
-                v.IE = v.UE / v.Z
-                v.S = v.UE * v.IE
-                v.convert()
-            })
-            m.set(n['current'], () => { // Here we primarily make Z fixed when I has changed
-                v.UE = v.IE * v.Z
-                v.S = v.UE * v.IE
-                v.convert()
-            })
-            m.set(n['impedance'], () => { // Here we primarily make U fixed when Z has changed
-                v.IE = v.UE / v.Z
-                v.S = v.UE * v.IE
-                v.convert_W()
-            })
-            m.set(n['power'], () => { // Here we primarily make Z fixed and let U, I able to change when S has changed
-                v.UE = Math.sqrt(v.S * v.Z)
-                v.IE = v.UE / v.Z
-                v.convert()
-            })
-            m.set(n['dBV'], () => {
-                v.UE = Math.pow(10, v.dBV / 20)
-                v.IE = v.UE / v.Z
-                v.S = v.UE * v.IE
-                v.convert()
-            })
-            m.set(n['dBu'], () => {
-                v.UE = Math.pow(10, (v.dBu + 20 * Math.log10(Math.sqrt(0.001 * 600))) / 20)
-                v.IE = v.UE / v.Z
-                v.S = v.UE * v.IE
-                v.convert()
-            })
-            m.set(n['dBW'], () => {
-                v.S = Math.pow(10, v.dBW / 10)
-                v.UE = Math.sqrt(v.S * v.Z)
-                v.IE = v.UE / v.Z
-                v.convert()
-            })
-            m.set(n['dBm'], () => {
-                v.S = Math.pow(10, (v.dBm - 30) / 10)
-                v.UE = Math.sqrt(v.S * v.Z)
-                v.IE = v.UE / v.Z
-                v.convert()
-            })
-            const read = () => {
-                {
-                    const W = value_keeper.WAVE_FORM
-                    const w = radio_button['wave-form']
-                }
-            }
-            const write = (skipped_inputs = new Set) => {
+        // The purpose of dual indices (compared with just number index): Lessen the modification of code when more inputs are inserted
+        const r = radio_button, n = number_input
+        let p = input.slice(0, 3) // radio button precursor
+        for (let i = 0; i < p.length; ++i) { r[i] = p[i], r[p[i].name] = {} } // index type 1 of 2: number; get ready for index type 2
+        for (let i = 0; i < p.length; ++i) { r[p[i].name][p[i].value] = p[i] } // index type 2 of 2: input.name and input.value
+        p = input.slice(3) // number input precursor
+        for (let i = 0; i < p.length; ++i) { n[i] = n[p[i].name] = p[i] }  // 2 types of indices: number and string
 
+        // add event handlers for the current value keeper
+        const v = current_value_keeper
+        const m = new Map // Element-Handler Map: Add event listeners. Canonical units: V, A, Ω, W (i.e., VA)
+        m.set(n['voltage'], () => { // Here we primarily make Z fixed when U has changed
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert()
+        })
+        m.set(n['current'], () => { // Here we primarily make Z fixed when I has changed
+            v.UE = v.IE * v.Z
+            v.S = v.UE * v.IE
+            v.convert()
+        })
+        m.set(n['impedance'], () => { // Here we primarily make U fixed when Z has changed
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert_W()
+        })
+        m.set(n['power'], () => { // Here we primarily make Z fixed and let U, I able to change when S has changed
+            v.UE = Math.sqrt(v.S * v.Z)
+            v.IE = v.UE / v.Z
+            v.convert()
+        })
+        m.set(n['dBV'], () => {
+            v.UE = Math.pow(10, v.dBV / 20)
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert()
+        })
+        m.set(n['dBu'], () => {
+            v.UE = Math.pow(10, (v.dBu + 20 * Math.log10(Math.sqrt(0.001 * 600))) / 20)
+            v.IE = v.UE / v.Z
+            v.S = v.UE * v.IE
+            v.convert()
+        })
+        m.set(n['dBW'], () => {
+            v.S = Math.pow(10, v.dBW / 10)
+            v.UE = Math.sqrt(v.S * v.Z)
+            v.IE = v.UE / v.Z
+            v.convert()
+        })
+        m.set(n['dBm'], () => {
+            v.S = Math.pow(10, (v.dBm - 30) / 10)
+            v.UE = Math.sqrt(v.S * v.Z)
+            v.IE = v.UE / v.Z
+            v.convert()
+        })
+        const read = () => {
+            {
+                const W = value_keeper.WAVE_FORM
+                const w = radio_button['wave-form']
+                v.wave_form = w['sine'].checked ? W.sine : w['triangular'].checked ? W.triangular : W.square
             }
-            for (const [ e, h ] of m) {
-                e.addEventListener('input', (event) => {
-                    read()
-                    h()
-                    write(new Set([ event.target ]))
-                })
+            {
+                
             }
+        }
+        const write = (skipped_inputs = new Set) => {
+
+        }
+        for (const [ e, h ] of m) {
+            e.addEventListener('input', (event) => {
+                read()
+                h()
+                write(new Set([ event.target ]))
+            })
         }
 
         // fire the corresponding event handlers and show an example of this unit converter utility
